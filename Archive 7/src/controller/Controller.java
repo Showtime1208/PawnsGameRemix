cd7ec0318749f2a99b26d07312f65a8ea1f7e887
Andres Matton-Conover <144743956+Showtime1208@users.noreply.github.com>
2025-04-03 15:19:29 -0400

    Delete Archive 7 directory

--- Archive 7/src/controller/Controller.java
+++
@@ -1,9 +0,0 @@
-package controller;
-
-
-/**
- * Controller Interface for the game. Not necessary yet.
- */
-public interface Controller {
-
-}
--- Archive 7/src/view/CellClickListener.java
+++
@@ -1,8 +0,0 @@
-package view;
-
-/**
- * Interface for the CellClickListener. Used as a listener for the board.
- */
-public interface CellClickListener {
-  void onCellClicked(int row, int col);
-}
