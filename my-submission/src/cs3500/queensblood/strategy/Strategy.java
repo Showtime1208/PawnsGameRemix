a16950ac8dcbb98443100fceac17697c43cf6235
Andres Matton-Conover <144743956+Showtime1208@users.noreply.github.com>
2025-04-03 16:34:55 -0400

    Delete my-submission directory

--- my-submission/src/cs3500/queensblood/strategy/Strategy.java
+++
@@ -1,8 +0,0 @@
-package cs3500.queensblood.strategy;
-
-import cs3500.queensblood.model.ImmutableGame;
-import cs3500.queensblood.model.ImmutableParticipant;
-
-public interface Strategy {
-  Move chooseMove(ImmutableGame model, ImmutableParticipant player);
-}
